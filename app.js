2867035170edc10f3c5ea8e756facd72d3694698
pavi24ps <pavi24ps@gmail.com>
2025-11-14 01:14:32 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+app.js
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Node.js CI/CD Pipeline Working! 🚀");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
